f85a81a3fafc7719d06b125e03b6a10a1d02a88a
Egor <workdukend@gmail.com>
2021-11-27 15:19:09 +0300

    FULL OS 678

---
+++ lab_6/src/timer.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <chrono>
+
+
+class Timer {
+ public:
+  Timer() = default;
+  ~Timer() = default;
+
+  void start_timer();
+  void stop_timer();
+  int get_time();
+  void heartbit(int time);
+
+
+
+ private:
+  bool is_timer_started = false;
+  std::chrono::steady_clock::time_point start_;
+  std::chrono::steady_clock::time_point finish_;
+};
